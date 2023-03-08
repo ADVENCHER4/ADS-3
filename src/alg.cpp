0bdc6666b755b9eaa2881d5c11bf2ba55bc0cc2b
ADVENCHER4 <73076392+ADVENCHER4@users.noreply.github.com>
2023-03-08 23:17:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,26 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int counter = 0;
+    int ind = size / 2;
+    int mid = arr[ind];
+    while (value < mid) {
+        ind /= 2;
+        mid = arr[ind];
+    }
+    while (mid > value) {
+        ind += ind / 2;
+        mid = arr[ind];
+    }
+    int lastInd = ind;
+    while (arr[ind] == value) {
+        counter++;
+        ind++;
+    }
+    ind = lastInd-1;
+    while (arr[ind] == value) {
+        counter++;
+        ind--;
+    }
+    return counter;
 }
